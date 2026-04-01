c8cd6fcb74e6f84cacf8a960a9a31af1cbe5a037
Andy Turner <A.G.D.Turner@gmail.com>
2026-04-01 12:57:21 +0100

    Updated documentation for 0.1. Included API in Sphinx documentation.

--- CHANGE_LOG.md
+++ CHANGE_LOG.md
@@ -3,20 +3,37 @@
 
 ## Table of Contents
 - [Overview](#overview)
-- [0.2.0](#020)
-- [0.1.0](#010)
+- [0.2](#02)
+- [0.1](#01)
 
 
 ## Overview
 Notable changes are to be documented in this file.
 
 
-## [0.2.0]
+## [0.2]
 - ...
 
-
-## [0.1.0]
-- data
+## [0.1]
+- README.md
+- DEVELOPMENT_HISTORY.md
+- CHANGE_LOG.md
+- ROADMAP.md
+- CODE_OF_CONDUCT.md
+- DEV_GUIDE.md
+- DEV_QUICKSTART.md
+- USER_GUIDE.md
+- CONTRIBUTORS
+- LICENSE
+- Makefile
+- requirements.txt
+- requirements-dev.txt
 - docs
-- sandbox
+- tests
+- scripts
+- src/cag
 - Simple ABM runs.
+
+## Pre-versioning
+- Exploratory code in modules and sandbox directories
+
--- docs/conf.py
+++ docs/conf.py
@@ -8,19 +8,28 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../src'))
+# Always prioritize the local src directory for imports
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
+if src_path not in sys.path:
+	sys.path.insert(0, src_path)
 
 # -- Project information -----------------------------------------------------
 
-project = 'GABM'
-copyright = '2026, GABM contributors, University of Leeds'
+project = 'Climate-Action-GABM'
+copyright = '2026, Climate-Action-GABM contributors, University of Leeds'
 author = 'Andy Turner'
 
 # -- General configuration ---------------------------------------------------
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'myst_parser']
 autosummary_generate = True
+
+# Ensure full API details in autodoc
+autodoc_default_options = {
+	'members': True,
+	'undoc-members': True,
+	'show-inheritance': True,
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
